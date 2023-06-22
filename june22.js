258797157591213b3d252b269b6d7d4723048e4a
Maria.D.Martinez <maria.d.martinez@disney.com>
2023-06-22 15:32:20 -0700

    fixed some spelling errors

--- june22.js
+++ june22.js
@@ -1,22 +1,23 @@
-// 1. create a variable called output and set it to
+// 1. Create a variable called output and set it to
 // "Hello World!" then have it displayed in the console
 
 
-// 2. create an empty function called greeting that takes a string input
-// you will pass in as the parametor your variable output that 
-// you created above 
+// 2. Create an empty function called greeting that takes a string input
+// You will pass in as the parameter your variable output that 
+// You created above 
 
 
-// 3. inside your empty function check if the input is eqaul to "Hello World!"
-// if so concatanate to it "My name is <insert your name>." and return the new value
-// if it doesnt match set the input to "Goodbye! I don't talk to strangers." and return the value
+// 3. Inside your empty function, check if the input is equal to "Hello World!"
+// If so concatenate to it "My name is <insert your name>." and return the new value
+// If it doesn't match, set the input to "Goodbye! I don't talk to strangers." and return the value
 
 
 
-// 4. create a new variable, call it result, and set it to the function (call the function)
-// remember to pass in your variable output into the function
-// print out the new variable value
-// reassign the variable result to call the function again, but this time pass in the variable wrongOutPut that is given below
+// 4. Create a new variable, call it result, and set it to the function (call the function)
+// Remember to pass in your variable output into the function
+// Print out the new variable value
+// Reassign the variable result to call the function again, 
+// but this time pass in the variable wrongOutPut that is given below
 
 
 
