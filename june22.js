1a8f04aca541e05b1c76daacaea1b87858292b51
Maria.D.Martinez <maria.d.martinez@disney.com>
2023-06-22 13:37:32 -0700

    testing

---
+++ june22.js
@@ -0,0 +1,27 @@
+// 1. create a variable called output and set it to
+// "Hello World!" then have it displayed in the console
+
+
+// 2. create an empty function called greeting that takes a string input
+// you will pass in as the parametor your variable output that 
+// you created above 
+
+
+// 3. inside your empty function check if the input is eqaul to "Hello World!"
+// if so concatanate to it "My name is <insert your name>." and return the new value
+// if it doesnt match set the input to "Goodbye! I don't talk to strangers." and return the value
+
+
+
+// 4. create a new variable, call it result, and set it to the function (call the function)
+// remember to pass in your variable output into the function
+// print out the new variable value
+// reassign the variable result to call the function again, but this time pass in the variable wrongOutPut that is given below
+
+
+
+let wrongOutPut = "This should return else value";
+
+
+
+
