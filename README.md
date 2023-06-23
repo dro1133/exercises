9e2607498f4d4e73fadd7ac11a85396c49f8ba0c
Maria.D.Martinez <maria.d.martinez@disney.com>
2023-06-22 19:39:26 -0700

    added a read me file

---
+++ README.md
@@ -0,0 +1,10 @@
+## Getting Started
+Clone repo on to your machine
+```shell
+git clone https://github.com/firme-coding/exercises.git
+```
+## How to run your code
+Type this command in your terminal
+```shell
+node june22.js
+```
--- june22.js
+++ june22.js
@@ -8,10 +8,8 @@
 
 
 // 3. Inside your empty function, check if the input is equal to "Hello World!"
-// If so concatenate to it "My name is <insert your name>." and return the new value
+// If so concatenate with "My name is <insert your name>." and return the new value
 // If it doesn't match, set the input to "Goodbye! I don't talk to strangers." and return the value
-
-
 
 // 4. Create a new variable, call it result, and set it to the function (call the function)
 // Remember to pass in your variable output into the function
@@ -20,8 +18,7 @@
 // but this time pass in the variable wrongOutPut that is given below
 
 
-
-let wrongOutPut = "This should return else value";
+result = greeting(wrongOutPut);
 
 
 
